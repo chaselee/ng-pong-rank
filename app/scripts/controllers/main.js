15fd04702a75741374bc06bffe7afde7794d6ce7
Ivan Sugerman <ivan@getambassador.com>
2015-10-14 21:28:46 -0400

    remove $scope

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,39 +8,44 @@
  * Controller of the pongRankApp
  */
 angular.module('pongRankApp')
-  .controller('MainCtrl', function ($scope, $localStorage) {
-    $scope.$storage = $localStorage;
-    $scope.$storage.players = $localStorage.players || [];
-    $scope.addPlayer = function () {
-      var sortedPlayers = $scope.$storage.players.sort(function(a, b) {
-          return a.id - b.id;
-      });
-      var newID = ( sortedPlayers.length ? sortedPlayers[sortedPlayers.length - 1].id : 0) + 1;
-      $scope.$storage.players.push(
-        {
-          id: newID,
-          firstName: $scope.$storage.player.firstName,
-          lastName: $scope.$storage.player.lastName,
-          points: $scope.$storage.player.points
-        }
-      );
-      $scope.$storage.player = {};
-    };
-    $scope.addPoints = function (id, points) {
-      $scope.$storage.players.some(function (player) {
-        if (player.id === id) {
-          player.points += points;
-          return true;
-        }
-      });
-    };
-    $scope.addGame = function () {
-      var pointDiff = $scope.$storage.game.pointsOne - $scope.$storage.game.pointsTwo;
-      if (pointDiff > 0) {
-        $scope.addPoints($scope.$storage.game.playerOne, pointDiff);
-      } else {
-        $scope.addPoints($scope.$storage.game.playerTwo, Math.abs(pointDiff));
-      }
-      $scope.$storage.game = {};
-    };
+  .controller('MainCtrl', MainCtrl);
+
+function MainCtrl($localStorage) {
+  this.$storage = $localStorage;
+  this.$storage.players = $localStorage.players || [];
+}
+
+MainCtrl.prototype.addPlayer = function() {
+  var sortedPlayers = this.$storage.players.sort(function(a, b) {
+      return a.id - b.id;
   });
+  var newID = ( sortedPlayers.length ? sortedPlayers[sortedPlayers.length - 1].id : 0) + 1;
+  this.$storage.players.push(
+    {
+      id: newID,
+      firstName: this.$storage.player.firstName,
+      lastName: this.$storage.player.lastName,
+      points: this.$storage.player.points
+    }
+  );
+  this.$storage.player = {};
+};
+
+MainCtrl.prototype.addPoints = function(id, points) {
+  this.$storage.players.some(function (player) {
+    if (player.id === id) {
+      player.points += points;
+      return true;
+    }
+  });
+};
+
+MainCtrl.prototype.addGame = function() {
+  var pointDiff = this.$storage.game.pointsOne - this.$storage.game.pointsTwo;
+  if (pointDiff > 0) {
+    this.addPoints(this.$storage.game.playerOne, pointDiff);
+  } else {
+    this.addPoints(this.$storage.game.playerTwo, Math.abs(pointDiff));
+  }
+  this.$storage.game = {};
+};
